c2f63deb7510ca10387538476064594cb4a113c0
Harsh Vardhan <harshvardhan1203jasrotia@gmail.com>
2026-03-10 15:06:37 +0530

    remove folder

--- level1/CalculateProfitLoss.java
+++
@@ -1,9 +0,0 @@
-public class CalculateProfitLoss {
-    public static void main(String[] args) {
-        int costPrice=129;
-        int sellingPrice=191;
-        int profit=sellingPrice-costPrice;
-        double percent=(profit*100)/costPrice;
-        System.out.println("The Cost Price is INR"+" "+costPrice+" "+"and Selling Price is INR"+" "+sellingPrice+"\n"+"The Profit is INR"+" "+profit+" "+"and the Profit Percentage is"+" "+percent);
-    }
-}
--- level1/DisplayResult.java
+++
@@ -1,9 +0,0 @@
-public class DisplayResult {
-    public static void main(String[] args) {
-        String name="Sam";
-        int rollNumber=1;
-        double percentMarks=99.99;
-        char result='P';
-        System.out.println("Displaying result:\n"+ name +" "+"with roll number"+rollNumber+"has scored"+percentMarks+"% Marks and Result is"+result);
-    }
-}
